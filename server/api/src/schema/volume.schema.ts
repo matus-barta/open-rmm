f58c7f3490410ab97658e2eeacb106070a62c8ae
Matus Barta <matus.barta64@gmail.com>
2022-02-06 20:15:17 +0100

    added check for VolumeID to be UUID

--- server/api/src/schema/volume.schema.ts
+++ server/api/src/schema/volume.schema.ts
@@ -3,7 +3,7 @@ import { number, object, string, TypeOf } from 'zod';
 export const createVolumeSchema = object({
 	body: object({
 		UUID: string({ required_error: 'UUID is missing' }).uuid(),
-		UniqueVolumeID: string({ required_error: 'UniqueVolumeID is missing' }),
+		UniqueVolumeID: string({ required_error: 'UniqueVolumeID is missing' }).uuid(),
 		VolumeName: string(),
 		VolumeLetter: string(),
 		HealthStatus: string({ required_error: 'HealthStatus is missing' }),
