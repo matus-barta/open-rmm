e56f148d514239af82e8fd617229e2ea041c2665
Matus Barta <matus.barta64@gmail.com>
2023-03-14 23:27:00 +0100

    rm - unused

--- server/src/lib/server/sequences/checkAuthUuid.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import log from '../utils/logger';
-
-const checkAuthUuid: Handle = async ({ event, resolve }) => {
-	log.info('auth');
-	return resolve(event);
-};
-
-export default checkAuthUuid;
