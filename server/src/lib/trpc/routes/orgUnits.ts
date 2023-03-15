c6e2644841ed6c19c9fbc310e4d098757ab7de4f
Matus Barta <matus.barta64@gmail.com>
2023-03-15 21:35:43 +0100

    rm unused import

--- server/src/lib/trpc/routes/orgUnits.ts
+++ server/src/lib/trpc/routes/orgUnits.ts
@@ -1,7 +1,6 @@
 import prisma from '$lib/server/db/prisma';
 import { logger } from '$lib/trpc/middleware/logger';
 import { t } from '$lib/trpc/t';
-import { z } from 'zod';
 
 export const orgUnits = t.router({
 	list: t.procedure.use(logger).query(() =>
