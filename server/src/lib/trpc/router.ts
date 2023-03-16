4fc3e23e804f6a85eb60551eea69fddc6d54d8c6
Matus Barta <matus.barta64@gmail.com>
2023-03-16 10:52:40 +0100

    load data from trpc api computer

--- server/src/lib/trpc/router.ts
+++ server/src/lib/trpc/router.ts
@@ -1,9 +1,11 @@
 import { t } from '$lib/trpc/t';
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { orgUnits } from '$lib/trpc/routes/orgUnits';
+import { computers } from '$lib/trpc/routes/computer';
 
 export const router = t.router({
-	orgUnits
+	orgUnits,
+	computers
 });
 
 export type Router = typeof router;
--- server/src/routes/dashboard/rmm/[slug]/+page.ts
+++ server/src/routes/dashboard/rmm/[slug]/+page.ts
@@ -1,11 +1,6 @@
+import { trpc } from '$lib/trpc/client';
 import type { PageLoad } from './$types';
-import type { ReadComputerInput } from '$lib/schema/computer.schema';
 
-export const load: PageLoad = async ({ fetch, params }) => {
-	const response = await fetch(`/api/computer/${params.slug}`);
-	const computers = (await response.json()) as ReadComputerInput[];
-
-	return {
-		computers
-	};
-};
+export const load: PageLoad = (event) => ({
+	computers: trpc(event).computers.list.query(event.params.slug)
+});
