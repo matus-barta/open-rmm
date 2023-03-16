46fcc9ca7996b9afdf63b5fc7fab598101c7de23
Matus Barta <matus.barta64@gmail.com>
2023-03-16 10:25:41 +0100

    impl. trpc computer api

---
+++ server/src/lib/trpc/routes/computer.ts
@@ -0,0 +1,24 @@
+import prisma from '$lib/server/db/prisma';
+import { logger } from '$lib/trpc/middleware/logger';
+import { t } from '$lib/trpc/t';
+import { z } from 'zod';
+
+export const computers = t.router({
+	list: t.procedure
+		.use(logger)
+		.input(z.string())
+		.query(({ input }) =>
+			prisma.computer.findMany({
+				where: {
+					OrgUnit: input
+				},
+				select: {
+					Id: true,
+					Uuid: true,
+					CreatedAt: true,
+					IsAdded: true,
+					IsAllowed: true
+				}
+			})
+		)
+});
