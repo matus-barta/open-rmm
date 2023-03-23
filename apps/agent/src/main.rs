fbf46f209adab19faf1013e54cf92dca680f4546
Matus Barta <matus.barta64@gmail.com>
2023-03-23 03:31:45 +0100

    this isnt that hard - last famous words

--- apps/agent/src/main.rs
+++ apps/agent/src/main.rs
@@ -1,11 +1,34 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UuidResponse {
+    #[serde(alias = "Uuid")]
+    uuid: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:5005/computer")
+    let uuid = Uuid::new_v4();
+    let client = reqwest::Client::new();
+
+    let mut map = HashMap::new();
+    map.insert("UUID", uuid.to_string());
+    map.insert(
+        "OneTimeKey",
+        "xiXnrDbYSXINR_nSC-wVphED5oE_bQYJPHjQU17lsjriZlOAgC1p94M3qF89CPt-".to_string(),
+    );
+
+    let res_json = client
+        .post("http://localhost:5005/computer")
+        .json(&map)
+        .send()
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<UuidResponse>()
         .await?;
-    println!("{:#?}", resp);
+
+    println!("{:#?}", res_json);
+
     Ok(())
 }
