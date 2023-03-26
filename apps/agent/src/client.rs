61c44ca30fe0d64a9cdab705b536817c150d4b42
Matus Barta <matus.barta64@gmail.com>
2023-03-27 01:11:42 +0200

    Agent: partially impl. sys info

--- apps/agent/src/client.rs
+++ apps/agent/src/client.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use sysinfo::{System, SystemExt};
 use uuid::Uuid;
 
 use crate::config::Config;
@@ -40,13 +41,93 @@ impl Client {
     }
 
     pub async fn report_system_info(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let uuid = device_uuid::load_uuid().await?;
+        let mut sys = System::new_all();
+        sys.refresh_all();
 
         let mut map = HashMap::new();
-        map.insert("UUID", uuid);
-        //map.insert();
 
-        hostname::get()?;
+        map.insert("UUID", device_uuid::load_uuid().await?);
+        map.insert("PendingReboot", "false".to_owned()); //TODO:
+        map.insert(
+            "ComputerName",
+            sys.host_name().unwrap_or_else(|| "<unknown>".to_owned()),
+        );
+        map.insert("LastBootUpTime", sys.boot_time().to_string()); //TODO: convert to ISO time
+        map.insert(
+            "OsVersion",
+            sys.os_version().unwrap_or_else(|| "<unknown>".to_owned()),
+        );
+        map.insert(
+            "OsName",
+            process_os_name(sys.long_os_version(), sys.os_version()),
+        );
+        map.insert(
+            "KernelVersion",
+            sys.kernel_version()
+                .unwrap_or_else(|| "<unknown>".to_owned()),
+        );
+        map.insert("Type", "<unknown>".to_owned()); //TODO:
+
+        println!(
+            "{}",
+            process_os_name(sys.long_os_version(), sys.os_version())
+        );
         Ok(())
     }
+}
+
+fn get_macos_version_name(major: u64, minor: u64) -> String {
+    match major {
+        13 => "Ventura".to_string(),
+        12 => "Monterey".to_string(),
+        11 => "Big Sur".to_string(),
+        10 => match minor {
+            15 => "Catalina".to_string(),
+            14 => "Mojave".to_string(),
+            13 => "High Sierra".to_string(),
+            12 => "Sierra".to_string(),
+            11 => "El Capitan".to_string(),
+            10 => "Yosemite".to_string(),
+            9 => "Mavericks".to_string(),
+            8 => "Mountain Lion".to_string(),
+            7 => "Lion".to_string(),
+            6 => "Snow Leopard".to_string(),
+            5 => "Leopard".to_string(),
+            4 => "Tiger".to_string(),
+            3 => "Panther".to_string(),
+            2 => "Jaguar".to_string(),
+            1 => "Puma".to_string(),
+            0 => "Cheetah".to_string(),
+            _ => "<unknown>".to_string(),
+        },
+        _ => "<unknown>".to_string(),
+    }
+}
+
+fn process_os_name(long_os_version: Option<String>, os_version: Option<String>) -> String {
+    let os_name = long_os_version.unwrap_or_else(|| "<unknown>".to_owned()); //MacOS 13.2.1
+
+    let sys_os_ver = os_version.unwrap_or_else(|| "<unknown>".to_owned()); //13.2.1
+    let version = parse_version(&sys_os_ver).unwrap_or_else(|| (0, 0, 0)); //13.2.1
+    let codename = get_macos_version_name(version.0, version.1); //Ventura
+
+    if os_name.contains("MacOS") {
+        format!("{}{}", os_name, codename)
+    } else {
+        os_name
+    }
+}
+
+fn parse_version(s: &str) -> Option<(u64, u64, u64)> {
+    let mut iter = s.trim().split_terminator('.').fuse();
+
+    let major = iter.next().and_then(|s| s.parse().ok())?;
+    let minor = iter.next().unwrap_or("0").parse().ok()?;
+    let patch = iter.next().unwrap_or("0").parse().ok()?;
+
+    if iter.next().is_some() {
+        return None;
+    }
+
+    Some((major, minor, patch))
 }
