5ecb462ab4259813e788abe466a9fcf3305a3747
Matus Barta <matus.barta64@gmail.com>
2023-05-01 22:45:13 +0200

    www : make ConfigBar component

---
+++ apps/www/src/lib/components/ConfigBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	const close = () => dispatch('close');
+	const refresh = () => dispatch('refresh');
+
+	export let title: string;
+</script>
+
+<div class="flex flex-col px-2 pt-1 pb-5 w-[512px] h-full bg-gray-700 absolute top-0 right-0">
+	<div class="flex justify-end pb-8">
+		<button on:click={refresh} class="p-1 button-ish">O</button>
+		<button on:click={close} class="p-1 button-ish">X</button>
+	</div>
+	<h1 class="text-right">{title}</h1>
+	<slot />
+</div>
