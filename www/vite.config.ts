f0b3f6943c3406c8ffe999b798cfd0e10a6886a1
Matus Barta <matus.barta64@gmail.com>
2023-09-01 22:29:07 +0200

    db & www : wasm working

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,13 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		Icons({
 			compiler: 'svelte'
-		})
+		}),
+		wasmPack("../db")
 	],
 	resolve: {
 		preserveSymlinks: true
