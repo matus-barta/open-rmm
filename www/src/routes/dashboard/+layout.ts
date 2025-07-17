7e860020f91bde2a6c82b8285a4929cfda7f1e09
Matus Barta <matus.barta64@gmail.com>
2025-07-17 20:48:11 +0200

    www : "optimization" 😅

--- www/src/routes/dashboard/+layout.ts
+++ www/src/routes/dashboard/+layout.ts
@@ -6,22 +6,22 @@ export const load: LayoutLoad = async ({ parent, fetch }) => {
 	const { supabase, user } = await parent();
 
 	const ghUrl = 'https://api.github.com/repos/matus-barta/Open_RMM';
-	const res = await fetch(ghUrl);
-
 	interface ghApi {
 		watchers: number;
 	}
 
-	const ghRes: ghApi = await res.json();
+	const watchers = (await fetch(ghUrl).then((res) => res.json() as Promise<ghApi>)).watchers;
 
 	if (user) {
 		const profile = await get_profile(supabase, user.id);
-		const tenant = await get_tenant(supabase, profile.tenant_id);
+		const tenant = async () => {
+			await get_tenant(supabase, profile.tenant_id);
+		};
 
 		return {
 			profile,
 			tenant,
-			ghStars: ghRes.watchers
+			ghStars: watchers
 		};
 	}
 };
