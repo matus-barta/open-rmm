43b6ce557eb6e02d9d3d82e611da4d1145bf4fbf
Matus Barta <matus.barta64@gmail.com>
2025-07-23 23:50:14 +0200

    www : add computer info panel

--- www/src/routes/dashboard/rmm/[slug]/+page.svelte
+++ www/src/routes/dashboard/rmm/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { invalidateAll } from '$app/navigation';
+	import { invalidateAll, onNavigate } from '$app/navigation';
 	import DialogBtn from '$lib/components/dialogButton.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
@@ -7,9 +7,9 @@
 	import * as Table from '$lib/components/ui/table/index.js';
 	import formatIsoDateTime from '$lib/utils/formatDateTime';
 	import type { PageProps } from './$types';
-	import Os from '$lib/components/Marks/os.svelte';
-	import MachineType from '$lib/components/Marks/machineType.svelte';
-	import Bool from '$lib/components/Marks/bool.svelte';
+	import Os from '$lib/components/marks/os.svelte';
+	import MachineType from '$lib/components/marks/machineType.svelte';
+	import Bool from '$lib/components/marks/bool.svelte';
 	import DrawerButton from '$lib/components/drawerButton.svelte';
 	import { Check, ClipboardCopy, RotateCw, SquarePen } from '@lucide/svelte';
 	import { add_computer } from '$lib/db/computer';
@@ -18,8 +18,8 @@
 	import * as Select from '$lib/components/ui/select/index.js';
 	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
-	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import log from '$lib/utils/logger';
+	import TooltipButton from '$lib/components/tooltipButton.svelte';
 
 	let { data }: PageProps = $props();
 
@@ -57,6 +57,10 @@
 		data.orgUnits.find((f) => f.orgUnit.uuid == selectedOrgUnitUuid)?.orgUnit.name ??
 			'Select an Org Unit'
 	);
+
+	onNavigate(() => {
+		selectedComputer = '';
+	});
 </script>
 
 <svelte:head>
@@ -66,8 +70,8 @@
 {#snippet orgUnitDialog()}
 	<div class="grid gap-4 py-4">
 		<div class="grid grid-cols-4 items-center gap-4">
-			<Label for="name" class="text-right">Name</Label>
-			<Input id="name" value="Pedro Duarte" class="col-span-3" />
+			<Label for="name" class="text-right">Org Unit Name</Label>
+			<Input id="name" value={data.orgUnitName} class="col-span-3" />
 		</div>
 		<div class="grid grid-cols-4 items-center gap-4">
 			<Label for="username" class="text-right">Username</Label>
@@ -115,6 +119,60 @@
 	</div>
 {/snippet}
 
+{#snippet showComputerDialog()}
+	{@const computer = data.computers.find((f) => f.uuid == selectedComputer)}
+	<div class="grid flex-1 auto-rows-min gap-6 px-4">
+		<div class="grid gap-3">
+			<Label for="uuid" class="text-right">UUID</Label>
+			<Input id="uuid" value={computer?.uuid} disabled />
+		</div>
+		<div class="grid grid-cols-2">
+			<Label for="isAllowed" class="text-right">Is Allowed</Label>
+			<Checkbox id="isAllowed" checked={computer?.is_allowed} disabled />
+		</div>
+		<div class="grid grid-cols-2">
+			<Label for="isAdded" class="text-right">Is Added</Label>
+			<Checkbox id="isAdded" checked={computer?.is_added} disabled />
+		</div>
+		<Separator />
+		<div class="grid gap-3">
+			<Label for="computerName" class="text-right">Computer Name</Label>
+			<Input id="computerName" value={computer?.system_info?.computer_name} disabled />
+		</div>
+		<div class="grid gap-3">
+			<Label for="osName" class="text-right">OS Name</Label>
+			<Input id="osName" value={computer?.system_info?.os_name} disabled />
+		</div>
+		<div class="grid gap-3">
+			<Label for="osVesion" class="text-right">OS Version</Label>
+			<Input id="osVersion" value={computer?.system_info?.os_version} disabled />
+		</div>
+		<div class="grid gap-3">
+			<Label for="osVesion" class="text-right">Kernel Version</Label>
+			<Input id="osVersion" value={computer?.system_info?.kernel_version} disabled />
+		</div>
+		<div class="grid gap-3">
+			<Label for="type" class="text-right">System Type</Label>
+			<Input id="type" value={computer?.system_info?.machine_type} disabled />
+		</div>
+		<Separator />
+		<div class="grid grid-cols-2">
+			<Label for="pendingReboot" class="text-right">Pending Reboot</Label>
+			<Checkbox
+				id="PendingReboot"
+				disabled
+				checked={computer?.system_info?.pending_reboot == null
+					? false
+					: computer?.system_info?.pending_reboot}
+			/>
+		</div>
+		<div class="grid gap-3">
+			<Label for="bootTime" class="text-right">Last Boot Time</Label>
+			<Input id="bootTime" value={computer?.system_info?.last_bootup_time} disabled />
+		</div>
+	</div>
+{/snippet}
+
 <div class="flex h-full flex-col gap-2">
 	<menubar class="flex h-auto flex-row justify-between rounded-lg border-2 p-1">
 		<left class="flex flex-row">
@@ -127,22 +185,29 @@
 				actionName="Create computer"
 				action={addComputer}
 			/>
-			<Button size="sm" variant="ghost">AAAAA</Button>
+			<showup class="flex flex-row">
+				<Separator orientation="vertical" class="mx-1" />
+				<DrawerButton
+					variant="ghost"
+					size="sm"
+					title="Computer details"
+					description="Computer details"
+					content={showComputerDialog}
+					disabled={selectedComputer == ''}
+				/>
+
+				<Button variant="ghost" size="sm" disabled={selectedComputer == ''}>Connect</Button>
+			</showup>
 		</left>
 		<right class="flex flex-row">
-			<Tooltip.Provider>
-				<Tooltip.Root>
-					<Tooltip.Trigger>
-						<Button size="sm" variant="ghost" onclick={invalidateAll}>
-							<RotateCw />
-						</Button>
-					</Tooltip.Trigger>
-					<Tooltip.Content side="bottom">
-						<p>Refresh</p>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			</Tooltip.Provider>
-
+			<TooltipButton
+				btnSize="sm"
+				side="bottom"
+				variant="ghost"
+				tooltip="Refresh"
+				Icon={RotateCw}
+				onclick={invalidateAll}
+			/>
 			<DialogBtn
 				variant="ghost"
 				size="sm"
